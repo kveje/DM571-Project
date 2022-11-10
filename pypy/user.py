bdb531f2c97bbe8d60ba33a13c265a682170cf6e
Nikolaj Dall <nidal20@student.sdu.dk>
2022-11-10 15:31:33 +0100

    api start

--- pypy/user.py
+++ pypy/user.py
@@ -1,9 +1,3 @@
 class User:
     def __init__(self, id, role, username, password):
         self.id = id
-        self.role = role #TODO: Snak omkring roller
-        self.username = username  #Maybe?
-        self._password = password  #Maybe?
-        
-        def set_password(self, password):
-            self._password = password
